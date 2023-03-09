b606b4544488b811c371f929b870a541f9501895
Mohammad Rajabzadeh <rajabzadehm43@gmail.com>
2023-03-09 14:55:48 +0330

    Add project files.

---
+++ IrProgrammerValidators/Validators/RequireIfTrueAttribute.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IrProgrammerValidators.Validators;
+
+public class RequireIfTrueAttribute : ValidationAttribute
+{
+
+    private readonly string _sourceParameterName;
+
+    public RequireIfTrueAttribute(string sourceParameterName)
+    {
+
+        _sourceParameterName = sourceParameterName;
+
+        if (string.IsNullOrEmpty(ErrorMessage))
+        {
+            ErrorMessage = $"وارد کردن این بخش در صورت True بودن {sourceParameterName} اجباری است";
+        }
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+
+        var instance = validationContext.ObjectInstance;
+        var sourceProperty = instance.GetType().GetProperty(_sourceParameterName);
+        var rawValue = sourceProperty?.GetValue(instance);
+
+        if (sourceProperty == null)
+        {
+            throw new ArgumentException($"Invalid Source Property, Please Enter Valid Source Property Name",
+                _sourceParameterName);
+        }
+
+        if (rawValue is bool == false)
+        {
+            throw new ArgumentException("Source Property Type Must Be Boolean Or bool");
+        }
+
+        var sourcePropertyValue = (bool?) rawValue;
+
+        if (sourcePropertyValue != true || value != null)
+        {
+            return ValidationResult.Success;
+        }
+
+        return new ValidationResult(ErrorMessage);
+    }
+}
---
+++ IrProgrammerValidators/Validators/RequireIfValue.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BetterHomeApi.Domains.Validators;
+
+public class RequireIfValue : ValidationAttribute
+{
+    private readonly string _sourceParameterName;
+    private readonly object _targetValue;
+
+    public RequireIfValue(string sourceParameterName, object targetValue)
+    {
+        _sourceParameterName = sourceParameterName;
+        _targetValue = targetValue;
+
+        if (string.IsNullOrEmpty(ErrorMessage))
+        {
+            ErrorMessage = $"وارد کردن این بخش در صورت True بودن {sourceParameterName} اجباری است";
+        }
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+
+        var instance = validationContext.ObjectInstance;
+        var sourceProperty = instance.GetType().GetProperty(_sourceParameterName);
+        var rawValue = sourceProperty?.GetValue(instance);
+
+        if (sourceProperty == null)
+        {
+            throw new ArgumentException($"Invalid Source Property, Please Enter Valid Source Property Name",
+                "sourceParameterName");
+        }
+
+        if (!Equals(rawValue, _targetValue) || value != null)
+        {
+            return ValidationResult.Success;
+        }
+
+        return new ValidationResult(ErrorMessage);
+    }
+}
