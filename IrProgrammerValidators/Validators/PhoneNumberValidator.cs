2dcc9cb871a2d181f8d13e4b12c0dd67732074d9
Mohammad Rajabzadeh <rajabzadehm43@gmail.com>
2023-08-04 16:03:10 +0330

    Add Phone Number Validator

---
+++ IrProgrammerValidators/Validators/PhoneNumberValidator.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace IrProgrammerValidators.Validators;
+
+public class PhoneNumberValidator : ValidationAttribute
+{
+    #region Ctor
+
+    private readonly Country _country;
+
+    public PhoneNumberValidator(Country country = Country.Iran)
+    {
+        _country = country;
+
+        if (string.IsNullOrEmpty(ErrorMessage))
+        {
+            ErrorMessage = "لطفا یک شماره تلفن معتبر وارد کنید";
+        }
+    }
+
+    #endregion
+
+    public override bool IsValid(object? value)
+    {
+        if (value.GetType() != typeof(string))
+        {
+            throw new ArgumentException("Value Is Not String, Value: " + value);
+        }
+
+        var correctValue = (string) value;
+        var validatorRegex = new Regex(@"09[0-9]{9}");
+        var pnSize = 11;
+
+        return correctValue.Length == pnSize && validatorRegex.IsMatch(correctValue);
+    }
+}
+
+public enum Country
+{
+    Iran
+}
